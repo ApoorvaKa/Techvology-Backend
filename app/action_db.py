1ba87841b18a302e05733ccd53cd2edb265e3405
Selina Ding <zd759@nyu.edu>
2022-11-13 18:59:41 -0500

    Added weekly_averages

--- app/action_db.py
+++ app/action_db.py
@@ -1,10 +1,11 @@
+import json
 from app import app
 from flask import request
 from flask_sqlalchemy import SQLAlchemy
 
-from datetime import datetime
+from datetime import datetime, timezone, timedelta
 from flask_cors import CORS
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:pass4now@localhost/techvology'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/techvology'
 
 db = SQLAlchemy(app)
 
@@ -12,7 +13,7 @@ class Action(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     carbon_output = db.Column(db.Integer, nullable=False)
-    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.now(timezone.utc))
 
     def __init__(self, title, carbon_output):
         self.title = title
@@ -72,3 +73,51 @@ def delete_action(id):
     db.session.delete(action)
     db.session.commit()
     return {"message": "Action deleted successfully"}
+
+### TESTING ###
+# create an event in the database for weekly average testing
+# @app.route('/actions', methods=['PATCH'])
+# def edit_action():
+#     title = request.json['title']
+#     carbon_output = request.json['carbon_output']
+#     month = request.json['month']
+#     day = request.json['day']
+#     action = Action(title, carbon_output)
+#     action.created_at = datetime(year=2022, month=month, day=day, hour=12, minute=15, second=21, tzinfo=timezone.utc)
+#     db.session.add(action)
+#     db.session.commit()
+#     return format_action(action)
+
+# get weekly averages
+@app.route('/weeklyAverages', methods=['GET'])
+def weekly_averages():
+    actions = Action.query.order_by(Action.created_at.asc()).all()
+    today = datetime.now()
+    sun_offset = (actions[0].created_at.weekday() - 6) % 7
+    sunday = actions[0].created_at - timedelta(days=sun_offset)
+    weeks = abs(sunday-today).days//7 + 1
+
+    averages = {}
+    index = 0
+    for _ in range(weeks):
+        next_sun = sunday + timedelta(days=7)
+        total = 0
+        not_same = True
+        
+        while actions[index].created_at < next_sun and index != -1 and not_same:
+            if actions[index].created_at.month == next_sun.month and actions[index].created_at.day == next_sun.day:
+                not_same = False
+            else: 
+                total += actions[index].carbon_output
+                index += 1
+                if index == len(actions):
+                    index = -1
+        
+        total /= 7
+
+        key = str(sunday.year) + '/' + str(sunday.month) + '/' + str(sunday.day)
+        averages[key] = round(total, 2)
+        sunday = next_sun
+
+    return json.dumps(averages)
+
